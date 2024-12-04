bfbf7eff36934fb3f78a1bfdac95f677fd12db4b
Andreas Henriksson <a.henriksson@gmail.com>
2024-12-04 13:15:05 +0100

    go03: part 1 solved
    main.go solves it imperatively, while regex.go solves the same thing
    using regex.
    
    The idea is that it's probably easier to extend main.go with some hacks
    to also handle corrupted strings (eg, mul which does not end with ')').

---
+++ go03/main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+	"unicode"
+)
+
+func findMul(s string) int {
+	return strings.Index(s, "mul(")
+}
+
+func trimDigit(s string) string {
+	len := 0
+	for _, char := range s {
+		if !unicode.IsDigit(char) {
+			break
+		}
+		len += 1
+	}
+
+	return s[:len]
+}
+
+func main() {
+	data, err := os.ReadFile("input.txt")
+	if err != nil {
+		fmt.Printf("Error reading file: %v\n", err)
+		return
+	}
+
+	mulSum := 0
+
+	idx := findMul(string(data))
+	for pos := 0; idx >= 0; idx = findMul(string(data[pos:])) {
+		idx += pos
+
+		//log.Printf("DEBUG: idx = %d\n", idx)
+		// find closing parenthesis (or first corrupted data)
+		end := 4
+		for _, val := range string(data[idx+4:]) {
+			if unicode.IsDigit(val) || val == ',' {
+				end += 1
+			} else {
+				break
+			}
+		}
+		//end := strings.Index(string(data[idx:]),")")
+
+		if string(data[idx+end:idx+end+1]) == ")" {
+			end += 1
+		} else {
+			log.Println("mul(digits,digits did not end with )")
+			pos = idx + end
+			continue
+		}
+	
+		mulArgs := string(data[idx:idx+end])
+		log.Printf("DEBUG: mulArgs=%s\n", mulArgs[4:])
+
+		// split on ,
+		parts := strings.Split(mulArgs[4:len(mulArgs)-1], ",")
+
+		if len(parts) != 2 {
+			log.Printf("WARNING (ignoring): Not to values in mul operation: %s\n", mulArgs)
+			pos = idx + end
+			continue
+		}
+
+		//parts[0] = trimDigit(parts[0])
+		//parts[1] = trimDigit(parts[1])
+
+		x, err := strconv.Atoi(parts[0])
+		if err != nil {
+			log.Printf("WARNING (ignoring): Failed to convert first value to integer")
+			pos = idx + end
+			continue
+		}
+
+		y, err := strconv.Atoi(parts[1])
+		if err != nil {
+			log.Printf("WARNING (ignoring): Failed to convert second value to integer")
+			pos = idx + end
+			continue
+		}
+
+		// multiply and summarize
+		mulSum += x*y
+		log.Printf("Multiplying %d x %d = %d (--> %d)\n", x, y, x*y, mulSum)
+
+		// continue searching from ....
+		pos = idx + end
+		//log.Printf("DEBUG: pos = %d\n", pos)
+	}
+
+	fmt.Printf("Summary: %d\n", mulSum)
+}
---
+++ go03/regex.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+func mul(s string) int {
+	// input: mul(123,456)
+	parts := strings.Split(s[4:len(s)-1], ",")
+	if len(parts) != 2 {
+		log.Fatalf("Not two parts!")
+	}
+
+	x, err := strconv.Atoi(parts[0])
+	if err != nil {
+		log.Fatalf("x failed atoi")
+	}
+
+	y, err := strconv.Atoi(parts[1])
+	if err != nil {
+		log.Fatalf("y failed atoi")
+	}
+
+	return x * y
+}
+
+func main() {
+	// Input string
+	input, err := os.ReadFile("input.txt")
+	if err != nil {
+		fmt.Printf("Error reading file: %v\n", err)
+		return
+	}
+
+	// Compile the regex pattern
+	pattern := `mul\(\d+,\d+\)`
+	re := regexp.MustCompile(pattern)
+
+	// Find all matches
+	matches := re.FindAllString(string(input), -1)
+
+	sum := 0
+	// Print matches
+	for _, match := range matches {
+		sum += mul(match)
+	}
+
+	fmt.Printf("Sum: %d\n", sum)
+}
+
