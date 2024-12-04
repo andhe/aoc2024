b440444ec715cb5bf9082ebf03c0d2fb1c1a6fa2
Andreas Henriksson <a.henriksson@gmail.com>
2024-12-04 15:35:09 +0100

    go03: part2: regex solves the second part

--- go03/regex.go
+++ go03/regex.go
@@ -38,16 +38,61 @@ func main() {
 	}
 
 	// Compile the regex pattern
-	pattern := `mul\(\d{1,3},\d{1,3}\)`
-	re := regexp.MustCompile(pattern)
+	mulpattern := `mul\(\d{1,3},\d{1,3}\)`
+	re := regexp.MustCompile(mulpattern)
 
 	// Find all matches
-	matches := re.FindAllString(string(input), -1)
+	muls := re.FindAllString(string(input), -1)
+	mulindices := re.FindAllStringIndex(string(input), -1)
 
+	dopattern := `do\(\)`
+	re = regexp.MustCompile(dopattern)
+	doindices := re.FindAllStringIndex(string(input), -1)
+
+	dontpattern := `don\'t\(\)`
+	re = regexp.MustCompile(dontpattern)
+	dontindices := re.FindAllStringIndex(string(input), -1)
+
+	mulEnabled := true
 	sum := 0
-	// Print matches
-	for _, match := range matches {
-		sum += mul(match)
+
+	for len(mulindices) > 0 {
+		donti := -1
+		doi := -1
+
+		if len(dontindices) > 0 && dontindices[0][0] < mulindices[0][0] {
+			donti = dontindices[0][0]
+		}
+		if len(doindices) > 0 && doindices[0][0] < mulindices[0][0] {
+			doi = doindices[0][0]
+		}
+
+		if donti >= 0 && doi >= 0 {
+			if donti < doi {
+				dontindices = dontindices[1:]
+				mulEnabled = false
+			} else {
+				doindices = doindices[1:]
+				mulEnabled = true
+			}
+		} else if donti < 0 && doi < 0 {
+			// ok
+		} else {
+			if donti < 0 {
+				doindices = doindices[1:]
+				mulEnabled = true
+			} else {
+				dontindices = dontindices[1:]
+				mulEnabled = false
+			}
+		}
+
+		if mulEnabled {
+			sum += mul(muls[0])
+		}
+
+		muls = muls[1:]
+		mulindices = mulindices[1:]
 	}
 
 	fmt.Printf("Sum: %d\n", sum)
