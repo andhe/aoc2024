6868abee0d50d89b8e7ff323e572195712e320cc
Andreas Henriksson <a.henriksson@gmail.com>
2024-12-12 13:08:14 +0100

    go09: part1: solved!
    Support fileid that are two (or more) digits!
    
    Defrag only works on character by character as long as fileid is < 10.
    This switches the code to avoid string conversions where possible.

--- go09/cmd/part1/main.go
+++ go09/cmd/part1/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"strconv"
-	"strings"
 	"os"
 )
 
@@ -12,71 +11,51 @@ const (
 	FREE
 )
 
-func defrag(line string) string {
-	y := len(line)-1
+func defragFs(fs []int) []int {
+	y := len(fs)-1
+	fsd := make([]int, len(fs))
 
-	data := line
+	for i:= 0; i<len(fs); i += 1 {
+		val := fs[i]
 
-	for idx, val := range line {
-		if val != '.' {
+		if val >= 0 {
+			fsd[i] = val
 			continue
 		}
 
-		// found first free space
-
-		// now find last file block
-		for ; line[y] == '.' && y >= 0 ; y -= 1 {
+		// found first free space, now find last block
+		for ; y > i && fs[y] == -1; y -= 1 {
 			continue
 		}
 
-		if y < idx {
-			break
-		}
-
-		// move block
-		/*
-		fmt.Println("---------------------------------------")
-		fmt.Printf("DEBUG: %s\n", data[:idx])
-		fmt.Printf("DEBUG: %s\n", string(line[y]))
-		fmt.Printf("DEBUG: %s\n", data[idx+1:y])
-		fmt.Printf("DEBUG: %s\n", ".")
-		fmt.Printf("DEBUG: %s\n", data[y+1:])
-		fmt.Println("---------------------------------------")
-		*/
-
-		data = data[:idx] + string(line[y]) + data[idx+1:y] + "." + data[y+1:]
-		y -= 1
-
-		//fmt.Printf("%s\n", data)
-
+		// switch places
+		fsd[i] = fs[y]
+		fs[y] = -1
 	}
 
-	return data
+	return  fsd
 }
 
-func checksum(data string) uint64 {
+func checksumFs(fs []int) uint64 {
 	sum := uint64(0)
-	fileid := 0
-	for _, val := range data {
-		if val == '.' {
+	pos := 0
+	for _, val := range fs {
+		if val < 0 {
 			continue
 		}
 
-		n, err := strconv.Atoi(string(val))
-		if err != nil {
-			panic("failed to atoi value")
-		}
-
-		sum += uint64(fileid * n)
-		fileid += 1
+		sum += uint64(val * pos)
+		pos += 1
 	}
 
 	return sum
-
 }
 
+
+
 func main() {
-//	input := "2333133121414131402"
+	var filesystem []int
+
 	input, err := os.ReadFile("input.txt")
 	if err != nil {
 		panic("failed to read input.txt")
@@ -88,10 +67,7 @@ func main() {
 	fileid := 0
 	state := FILE
 
-	output := ""
-
 	for _, val := range input {
-		//fmt.Printf("DEBUG: val=%c (%d)\n", val, val)
 		n, err := strconv.Atoi(string(val))
 		if err != nil {
 			panic("failed to atoi value")
@@ -99,20 +75,22 @@ func main() {
 
 		switch state {
 		case FILE:
-			output += fmt.Sprintf("%s", strings.Repeat(strconv.Itoa(fileid), n))
+			for i := 0 ; i < n; i += 1 {
+				filesystem = append(filesystem, fileid)
+			}
 			fileid+=1
 			state = FREE
 		case FREE:
-			output += fmt.Sprintf("%s", strings.Repeat(".", n))
+			for i := 0 ; i < n; i += 1 {
+				filesystem = append(filesystem, -1)
+			}
 			state = FILE
 		default:
 			panic("not supposed to happen")
 		}
 	}
-	fmt.Printf("%s\n", output)
-	d := defrag(output)
-	fmt.Println(d)
+	fsd := defragFs(filesystem)
 
-	checksum := checksum(d)
-	fmt.Printf("Checksum: %v\n", checksum)
+	checksumFs := checksumFs(fsd)
+	fmt.Printf("Checksum: %v\n", checksumFs)
 }
