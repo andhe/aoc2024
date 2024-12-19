c24453a54c3b20e9f859d55fe016ad68340103e2
Andreas Henriksson <a.henriksson@gmail.com>
2024-12-19 14:55:32 +0100

    go12: part1 -- identify plots

--- go12/main.go
+++ go12/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"slices"
 )
 
 type pos struct {
@@ -11,9 +12,41 @@ type pos struct {
 	y int
 }
 
+func checkConnected(val byte, x int, y int, matrix []string, plotsPtr *[][]pos) bool {
+	plots := *plotsPtr
+
+
+	line := matrix[x]
+	if y > 0 {
+		yleftval := line[y-1]
+		if yleftval == val {
+			for n,s := range plots {
+				if slices.Contains(s, pos{x,y-1}) {
+					plots[n] = append(plots[n], pos{x,y})
+					return true
+				}
+			}
+		}
+	}
+	if x > 0 {
+		upline := matrix[x-1]
+		xupval := upline[y]
+		if xupval == val {
+			for n,s := range plots {
+				if slices.Contains(s, pos{x-1,y}) {
+					plots[n] = append(plots[n], pos{x,y})
+					return true
+				}
+			}
+		}
+	}
+	// no point in checking in other directions, because we only check what we've already processed
+	return false
+}
+
 func main() {
 	var matrix []string
-	var plots []map[rune][]pos
+	var plots [][]pos
 
 	// populate matrix
 	file, err := os.Open("input.txt")
@@ -34,5 +67,19 @@ func main() {
 	}
 
 	// find plots
+	for x,line := range matrix {
+		for y,val := range line {
+			if !checkConnected(byte(val), x, y, matrix, &plots) {
+				// new plot
+				p := make([]pos, 0, 1)
+				p = append(p, pos{x,y})
+				plots = append(plots, p)
+			}
+		}
+	}
+
+	for _, p := range plots {
+		fmt.Println("%s: %v\n", string(matrix[p[0].x][p[0].y]), p)
+	}
 	
 }
